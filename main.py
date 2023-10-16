d3006e4634c978519d8ceb51e82bd493d53e12b1
varamu <147985545+varamu@users.noreply.github.com>
2023-10-16 13:56:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,32 +3,15 @@ from langchain import PromptTemplate
 from langchain.llms import OpenAI
 
 template = """
-    Below is a content that may be poorly worded.
-    Your goal is to:
-    - Properly format the content into product description
-    - Convert the input text to a specified tone
-    - Convert the input text to a specified dialect
-
-    Here are some examples different age groups:
-    - Formal: We went to Barcelona for the weekend. We have a lot of things to tell you.
-    - Informal: Went to Barcelona for the weekend. Lots to tell you.  
-
-    Here are some examples of words in different dialects:
-    - American: French Fries, cotton candy, apartment, garbage, cookie, green thumb, parking lot, pants, windshield
-    - British: chips, candyfloss, flag, rubbish, biscuit, green fingers, car park, trousers, windscreen
-
-    Example Sentences from each dialect:
-    - American: I headed straight for the produce section to grab some fresh vegetables, like bell peppers and zucchini. After that, I made my way to the meat department to pick up some chicken breasts.
-    - British: Well, I popped down to the local shop just the other day to pick up a few bits and bobs. As I was perusing the aisles, I noticed that they were fresh out of biscuits, which was a bit of a disappointment, as I do love a good cuppa with a biscuit or two.
-
-    Please start the content with a warm introduction. Add the introduction if you need to.
-    
-    Below is the content, age group, and hobby:
-    Age group: {agegroup}
-    Main Hobby: {hobby}
-    Content: {content}
-    
-    YOUR RESPONSE:
+    You are a marketing copywriter with 20 years of experience. You are analyzing customer's background to write personalized product description that only this customer will receive. 
+    PRODUCT input text: {content}
+    CUSTOMER age group (y): {agegroup}
+    CUSTOMER main Hobby: {hobby}
+   
+    TASK: {Write a product description that is tailored into this customer's Age group and hobby. Use age group specific slang.}
+    FORMAT: {Present the result in the following order: (PRODUCT DESCRIPTION), (USE CASE)}
+    PRODUCT DESCRIPTION: {describe the product in 5 sentences}
+    USE CASE: {describe in 5 sentences an example weekend activity taking into account hobby {hobby} and age {agegroup}}
 """
 
 prompt = PromptTemplate(
